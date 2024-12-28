9e2b903e171fedfa02a3a4e5e5e213f7632e3af9
hantabaru1014 <hantabaru1014@gmail.com>
2024-12-29 00:57:36 +0900

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# SecureRenderToTextureAsset
+
+AFK状態のワールドにある `RenderToTextureAsset` がフォーカス中のワールドのスクショをレンダリングしてしまうのを防ぐMod
+
+Mod that prevents `RenderToTextureAsset` in an AFK world from rendering screenshots of the focused world.
+
+## Installation
+1. Install [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader).
+2. Place [SecureRenderToTextureAsset.dll](https://github.com/hantabaru1014/SecureRenderToTextureAsset/releases/latest/download/SecureRenderToTextureAsset.dll) into your `rml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\Resonite\rml_mods` for a default install. You can create it if it's missing, or if you launch the game once with ResoniteModLoader installed it will create the folder for you.
+3. Start the game. If you want to verify that the mod is working you can check your Resonite logs.
---
+++ SecureRenderToTextureAsset.cs
@@ -0,0 +1,46 @@
+﻿using FrooxEngine.ProtoFlux;
+using HarmonyLib;
+using ProtoFlux.Runtimes.Execution.Nodes.FrooxEngine.Rendering;
+using ResoniteModLoader;
+using System;
+using System.Reflection;
+
+namespace SecureRenderToTextureAsset;
+
+public class SecureRenderToTextureAsset : ResoniteMod
+{
+    public override string Name => "SecureRenderToTextureAsset";
+    public override string Author => "hantabaru1014";
+    public override string Version => "1.0.0";
+    public override string Link => "https://github.com/hantabaru1014/SecureRenderToTextureAsset";
+
+    public override void OnEngineInit()
+    {
+        Harmony harmony = new Harmony("dev.baru.resonite.SecureRenderToTextureAsset");
+        harmony.PatchAll();
+    }
+
+    [HarmonyPatch]
+    class RunAsync_Prefix
+    {
+        static Type TargetClass()
+        {
+            return AccessTools.FirstInner(typeof(RenderToTextureAsset), t => t.Name.Contains("RunAsync"));
+        }
+
+        static MethodBase TargetMethod()
+        {
+            return AccessTools.Method(TargetClass(), "MoveNext");
+        }
+
+        public static bool Prefix(FrooxEngineContext ___context)
+        {
+            if (___context.Group.World.SessionId != ___context.Engine.WorldManager.FocusedWorld.SessionId)
+            {
+                Msg($"Blocked invalid render! node_world: {___context.Group.World.Name}, focused: {___context.Engine.WorldManager.FocusedWorld.Name}");
+                return false;
+            }
+            return true;
+        }
+    }
+}
